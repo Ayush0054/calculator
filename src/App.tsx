4d9005b8e7c14bd7deef022a101c86b97b44ad8b
Ayush0054 <ayush5aprill@gmail.com>
2024-01-29 19:52:22 +0530

    update: added keyboard listener

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Keypad from "./components/keypad";
 function App() {
   const [result, setResult] = useState("");
 
-  const handleKeyPress = (event) => {
+  const handleKeyPress = (event: KeyboardEvent) => {
     const key = event.key;
 
     if (key === "Enter") {
@@ -25,7 +25,7 @@ function App() {
     }
   };
 
-  // Add and remove the event listener
+  // Add and remove the event listener for keyboard event
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
     return () => document.removeEventListener("keydown", handleKeyPress);
@@ -53,19 +53,21 @@ function App() {
     }
   };
 
+  //c
   const reset = () => {
     setResult("");
   };
 
+  //ce
   const backspace = () => {
     setResult(result.slice(0, -1));
   };
 
   return (
-    <div className=" h-[100vh]  flex flex-col justify-center md:flex md:justify-center md:items-center p-5   md:m-16 ">
-      <div className=" md:shadow-xl p-4  flex flex-col items-center  rounded-xl">
+    <div className=" h-[100vh] md:h-[80vh]  flex flex-col justify-center md:flex md:justify-start md:items-center p-5   md:m-16 ">
+      <div className=" md:shadow-xl bg-white p-4  flex flex-col items-center  rounded-xl">
         <div className="bg-green-100 md:min-h-[70px] min-h-[110px] max-w-[350px] w-full  p-4 rounded-lg mb-4   ">
-          <h1 className="text-right text-3xl font-medium text-black break-words  ">
+          <h1 className="text-right text-3xl font-bold text-black break-words  ">
             {result || "0"}
           </h1>
         </div>
